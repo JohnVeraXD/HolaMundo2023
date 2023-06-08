c15017bef3bfcc5c98d122bce0c4ef8044224322
kevinbeles1999 <kbeltrane@uteq.edu.ec>
2023-06-08 16:17:50 -0500

    Habilitar Boton Enviar

--- app/src/main/java/com/example/holoamundo2023/MainActivity.java
+++ app/src/main/java/com/example/holoamundo2023/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.holoamundo2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void btnEntrar(View view){
+        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
+        Toast.makeText(this.getApplicationContext(), txtNombre.getText().toString(), Toast.LENGTH_SHORT).show();
+
+
     }
 }
