1afb4c41d54f6a1e25e63da52501c2dec94eae36
kevinbeles1999 <kbeltrane@uteq.edu.ec>
2023-06-15 13:02:43 -0500

    Provando mas botones y mandar menssajes de una actividad a otra

--- app/src/main/java/com/example/holoamundo2023/MainActivity.java
+++ app/src/main/java/com/example/holoamundo2023/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.holoamundo2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,9 +20,45 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void btnEntrar(View view){
-        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
-        Toast.makeText(this.getApplicationContext(), txtNombre.getText().toString(), Toast.LENGTH_SHORT).show();
 
+        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
+        //Toast.makeText(this.getApplicationContext(), txtNombre.getText().toString(), Toast.LENGTH_SHORT).show();
+
+        //Primero se saca la referencia aqui
+        EditText txtPassword = (EditText)findViewById(R.id.txtPassword);
+        RadioButton rbMascu = (RadioButton)findViewById(R.id.rbMascu);
+        Switch swnotificaciones = (Switch)findViewById(R.id.swnotificaciones);
+        EditText txtFechaDate = (EditText)findViewById(R.id.txtFechaDate);
+
+        //Despues se procesa aca
+        String nombre =txtNombre.getText().toString();
+        String Password = txtPassword.getText().toString();
+        //Para sacar el genero seleccionado
+        String genero = rbMascu.isChecked()?"Masculino":"Femenino";
+        //Sacar si recibe o no notificaciones
+        String notificaciones = swnotificaciones.isChecked()?"SI":"NO";
+        //Sacar la fecha de nacimiento
+        String fechaNaci = txtFechaDate.getText().toString();
+
+        Toast.makeText(this.getApplicationContext(),
+                "Nombre:" + nombre + "," +
+                        "Password: " + Password + "," +
+                        "Genero" + genero + "," +
+                        "Notificacion" + notificaciones + "," +
+                        "FechaNacimiento" + fechaNaci,
+                Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+        Bundle b = new Bundle();
+
+        b.putString("Nombre",nombre);
+        b.putString("Password",Password);
+        b.putString("Genero",genero);
+        b.putString("Notifica",notificaciones);
+        intent.putExtras(b);
+
+        startActivity(intent);
 
     }
 }
---
+++ app/src/main/java/com/example/holoamundo2023/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.holoamundo2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
